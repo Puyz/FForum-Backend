a19f7b38f9a08a638e7f40bfa9c28c96544cc38c
puyz <puyzsoft@gmail.com>
2023-06-01 20:58:02 +0300

    updated

--- Repositories/Abstracts/ICategoryRepository.cs
+++ Repositories/Abstracts/ICategoryRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Core.Repositories;
 using Entities.Concretes;
 using Entities.DTOs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repositories.Abstracts
 {
--- Services/Abstracts/ICategoryService.cs
+++ Services/Abstracts/ICategoryService.cs
@@ -1,11 +1,6 @@
 ﻿using Core.Utilities.Results.Abstracts;
 using Entities.Concretes;
 using Entities.DTOs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Services.Abstracts
 {
